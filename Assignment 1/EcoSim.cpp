3f427ca11c1b693d986f5fd252866498876f8072
Evan Voordeckers <evan.voordeckers@yahoo.com>
2019-08-29 13:25:13 -0600

    Added comments

--- Assignment 1/EcoSim.cpp
+++ Assignment 1/EcoSim.cpp
@@ -1,3 +1,9 @@
+/*
+  This app runs a simulation based on a given starting population number
+  of foxes and rabbits. Then prints out results in a graph.
+  Written by Evan Voordeckers. UID: u1087559
+*/
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -9,6 +15,10 @@ void plotCharacter(int number, char c);
 void plotPopulations(double numOfRabbits, double numOfFoxes, double factor);
 int& incrementCounter(int& counter);
 
+/*
+  Main entry point for the program that gets the initial population
+  numbers for running the simulation.
+*/
 int main()
 {
   double rabbitPop;
@@ -34,30 +44,32 @@ int main()
 }
 
 
+/*
+  Update population numbers based on formulas given in the assignment
+*/
 void updatePopulations(double g, double p, double c, double m, double K, double& numRabbits, double& numFoxes)
 {
   double rabbitChange = g * numRabbits * (1 - numRabbits/K) - p * numRabbits * numFoxes;
   double foxChange = c * p * numRabbits * numFoxes - m * numFoxes;
 
-  // std::cout << "Rabbit Change = " << rabbitChange << '\n';
-  // std::cout << "Fox Change = " << foxChange << '\n';
-
-
   numRabbits += rabbitChange;
   numFoxes += foxChange;
-  //
-  // std::cout << "Rabbit num = " << numRabbits << '\n';
-  // std::cout << "Fox num = " << numFoxes << '\n';
 }
 
+/*
+  Runs the simulation based with the given amount of iterations
+*/
 void runSimulation(int iterations, double rabbits, double foxes)
 {
   plotPopulations(rabbits, foxes, 0.1);
 
+  // Loop through iterations and update/print the population numbers.
   int i = iterations;
   for (i; i > 0; i--)
   {
     updatePopulations(0.2, 0.0022, 0.6, 0.2, 1000.0, rabbits, foxes);
+
+    // Error checking
     if(rabbits < 0 || foxes < 0)
     {
       std::cerr << "Rabbit or fox population dropped below 0" << '\n';
@@ -68,6 +80,10 @@ void runSimulation(int iterations, double rabbits, double foxes)
   }
 }
 
+/*
+  Prints out the given character after printing the
+  given number of spaces
+*/
 void plotCharacter(int number, char c)
 {
   for(int i = 1; i < number; incrementCounter(i))
@@ -78,6 +94,10 @@ void plotCharacter(int number, char c)
   std::cout << c;
 }
 
+/*
+  Plots the population of given number of rabbits and
+  foxes. Multiplies the pop numbers by the factor given.
+*/
 void plotPopulations(double numOfRabbits, double numOfFoxes, double factor)
 {
   if(numOfRabbits < numOfFoxes)
@@ -95,6 +115,10 @@ void plotPopulations(double numOfRabbits, double numOfFoxes, double factor)
 }
 
 
+/*
+  Counter incrementer for practiving pass by reference
+  as opposed to pass by value
+*/
 int& incrementCounter(int& counter)
 {
   counter += 1;
