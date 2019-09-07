700a5ed393bb8852dd710d5270132b9483b40abf
Evan Voordeckers <evan.voordeckers@yahoo.com>
2019-09-07 15:06:16 -0600

    structure of assignment is built out

---
+++ Assignment02/HaruPDF.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "hpdf.h"
+
+class HaruPDF
+{
+public:
+  HPDF_Doc newPDF(HPDF_Error_Handler errHandler, void *user_data)
+  {
+
+  }
+}
